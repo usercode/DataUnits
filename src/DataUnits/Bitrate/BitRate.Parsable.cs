d543a683981285760943c22a72e3e0ed4217a91b
usercode <2958488+usercode@users.noreply.github.com>
2025-04-29 21:50:46 +0200

    Refactoring (2)

--- src/DataUnits/Bitrate/BitRate.Parsable.cs
+++ src/DataUnits/Bitrate/BitRate.Parsable.cs
@@ -9,9 +9,9 @@ public partial struct BitRate : IParsable<BitRate>
     [GeneratedRegex(@"^\s*(?<value>-?[0-9]+(\p{P}[0-9]+)?)\s*(?<symbol>[a-z]{1,2}ps)\s*$", RegexOptions.IgnoreCase)]
     private static partial Regex Regex();
 
-    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out BitRate value)
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? formatProvider, [MaybeNullWhen(false)] out BitRate value)
     {
-        return Parsable.TryParse<BitRate, BitUnit>(Regex(), s, provider, out value);
+        return Parsable.TryParse<BitRate, BitUnit>(Regex(), s, formatProvider, out value);
     }
 
     public static bool TryParse([NotNullWhen(true)] string? s, [MaybeNullWhen(false)] out BitRate value)
@@ -19,9 +19,9 @@ public partial struct BitRate : IParsable<BitRate>
         return TryParse(s, null, out value);
     }
 
-    public static BitRate Parse(string? s, IFormatProvider? provider = null)
+    public static BitRate Parse(string? s, IFormatProvider? formatProvider = null)
     {
-        if (TryParse(s, provider, out BitRate value))
+        if (TryParse(s, formatProvider, out BitRate value))
         {
             return value;
         }
--- src/DataUnits/ByteSize/ByteSize.Parsable.cs
+++ src/DataUnits/ByteSize/ByteSize.Parsable.cs
@@ -9,9 +9,9 @@ public partial struct ByteSize : IParsable<ByteSize>
     [GeneratedRegex(@"^\s*(?<value>-?[0-9]+(\p{P}[0-9]+)?)\s*(?<symbol>[a-z]{1,2})\s*$", RegexOptions.IgnoreCase)]
     private static partial Regex Regex();
 
-    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out ByteSize value)
+    public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? formatProvider, [MaybeNullWhen(false)] out ByteSize value)
     {
-        return Parsable.TryParse<ByteSize, ByteUnit>(Regex(), s, provider, out value);
+        return Parsable.TryParse<ByteSize, ByteUnit>(Regex(), s, formatProvider, out value);
     }
 
     public static bool TryParse([NotNullWhen(true)] string? s, [MaybeNullWhen(false)] out ByteSize value)
@@ -19,9 +19,9 @@ public partial struct ByteSize : IParsable<ByteSize>
         return TryParse(s, null, out value);
     }
 
-    public static ByteSize Parse(string? s, IFormatProvider? provider = null)
+    public static ByteSize Parse(string? s, IFormatProvider? formatProvider = null)
     {
-        if (TryParse(s, provider, out ByteSize value))
+        if (TryParse(s, formatProvider, out ByteSize value))
         {
             return value;
         }
